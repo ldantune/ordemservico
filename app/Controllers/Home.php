eae3a70d5e9f00172054d11bdcb252c96b7bdaf8
Lucas Antunes <lucasdanielantunes@gmail.com>
2022-11-29 14:45:28 -0300

    Layout da aplicação

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'titulo' => 'Home'
+        ];
+        return view('Home/index', $data);
     }
 }
---
+++ app/Views/Home/index.php
@@ -0,0 +1,20 @@
+<?php echo $this->extend('Layout/principal') ?>
+
+<?php echo $this->section('titulo') ?>
+    <?php echo $titulo; ?>
+<?php echo $this->endSection() ?>
+
+
+<?php echo $this->section('estilos') ?>
+    
+<?php echo $this->endSection() ?>
+
+
+<?php echo $this->section('conteudo') ?>
+    
+<?php echo $this->endSection() ?>
+
+
+<?php echo $this->section('scripts') ?>
+    
+<?php echo $this->endSection() ?>
